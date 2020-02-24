0eba5463a649432f6ebb29a5455c1775fa6aae54
Simon Dierl <simon.dierl@cs.tu-dortmund.de>
2020-02-24 18:18:26 +0100

    initial attempt

---
+++ README.md
@@ -0,0 +1,15 @@
+### The CVC4 Azure Build
+
+[CVC4](https://cvc4.github.io/) is a modular SMT solver that wraps numerous other open source packages. Unfortunately,
+the authors only provide static binary builds of the solver executable, making reuse difficult. This project aims to use
+Azure Pipelines to provide these artifacts.
+
+#### Structure
+
+The build wrapper logic resides in `build.sh`, while necessary patches to the CVC4 sources are collected in the
+`.patch` file. 
+
+#### License
+
+CVC4 is licensed under the various licenses. The support files in this project are licensed under the
+[ISC License](https://opensource.org/licenses/ISC).
---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Simon Dierl <simon.dierl@cs.tu-dortmund.de>
+# SPDX-License-Identifier: ISC
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
+# granted, provided that the above copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+# AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+set -euxo pipefail
+
+CVC4_VERSION=1.7
+
+prefix="$(pwd)/build"
+patch="$(pwd)/cvc4-${CVC4_VERSION}.patch"
+
+git clone https://github.com/CVC4/CVC4.git --branch ${CVC4_VERSION} src
+pushd CVC4
+
+git apply "${patch}"
+
+for dependency in antlr-3.4 gmp abc cadical cln cryptominisat drat2er glpk-cut-log lfsc symfpu; do
+  contrib/get-$dependency
+done
+
+./configure.sh --gpl \
+  --abc --cadical --cln --cryptominisat --drat2er --lfsc --glpk --symfpu \
+  --language-bindings=java --prefix="${prefix}"
+pushd build
+make -j "$(nproc)" install
+popd
+
+popd
